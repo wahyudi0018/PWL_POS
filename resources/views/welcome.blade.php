913de10fac3d0821270f0d858cb060cfab3ec39a
wahyudi0018 <wazncourrier@gmail.com>
2024-04-04 13:46:11 +0700

    Layouting AdminLTE

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,38 @@
+{{-- @extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <div class="card-body">
+        <form>
+            <div class="row">
+                <div class="col-sm-6">
+                    <!-- input teks -->
+                    <div class="form-group">
+                        <label>Level id</label>
+                        <input type="text" class="form-control" placeholder="id">
+                    </div>
+                    <button type="submit" class="btn btn-info">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+@stop --}}
+
+@section('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+<script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
