fe8605cecc5c68098f514998d7522b2328362266
wahyudi0018 <wazncourrier@gmail.com>
2024-05-13 21:59:31 +0700

    Penambahan informasi file-upload

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -11,6 +11,25 @@ class FileUploadController extends Controller
     }
 
     public function prosesFileUpload(Request $request){
-        return view('file-upload');
+        // return "Pemrosesan file upload di sini";
+        // dump($request->berkas);
+        if($request->hasFile('berkas'))
+        {
+            echo "path(): ".$request->berkas->path();
+            echo "<br>";
+            echo "extension(): ".$request->berkas->extension();
+            echo "<br>";
+            echo "getClientOriginalExtension(): ".$request->berkas->getClientOriginalExtension();
+            echo "<br>";
+            echo "getMimeType(): ".$request->berkas->getMimeType();
+            echo "<br>";
+            echo "getClientOriginalName(): ".$request->berkas->getClientOriginalName();
+            echo "<br>";
+            echo "getSize(): ".$request->berkas->getSize();
+            echo "<br>";
+        } else
+        {
+            echo "Tidak ada berkas yang diupload";
+        }
     }
 }
